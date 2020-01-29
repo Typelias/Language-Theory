6015723ba17adb544f893dfd7462c7f1fd8cf2a7
typelias <98ille@gmail.com>
2020-01-29 13:52:01 +0100

    Worked on lab1

--- Labb1/main.cpp
+++ Labb1/main.cpp
@@ -1,27 +1,92 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
+#include <string>
+#include <stack>
+#include <iomanip>
 
-struct Node {
-    std::string val;
-    Node* left;
-    Node* right;
-    Node* parent;
-    Node(std::string val): val(val) {}
+enum Type{
+    Integer = 0,
+    Operator = 1
 };
+
+struct Node{
+    Type type;
+    std::string val;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+Node* buildTree(std::string::reverse_iterator begin, std::string::reverse_iterator end) {
+    if(end>= begin){
+        std::string::reverse_iterator it = std::find(begin, end,'-');
+        if(it == end) {
+            std::string::reverse_iterator it = std::find(begin, end,'+');
+            if(it == end) {
+                std::string::reverse_iterator it = std::find(begin, end,'*');
+                if(it == end) {
+                    std::string::reverse_iterator it = std::find(begin, end,'/');
+                    if(it == end) {
+                        std::cout << "Hello\n";
+                        Node* temp = new Node;
+                        std::string tempString = std::string(begin, end);
+                        std::reverse(tempString.begin(), tempString.end());
+                        temp->val = tempString;
+                        temp->type = Integer;
+                        //std::cout << tempString << "\n";
+                        return temp;
+                    }
+                }
+            }
+        }
+        Node* temp = new Node;
+        std::cout << *it << "\n";
+        temp->val = *it;
+        temp->type = Operator;
+        temp->left = buildTree(begin, it - 1);
+        temp->right = buildTree(it+1, end);
+
+        return temp;
+    }
+    return nullptr;
+}
+
+void del(Node* root){
+    if(root->left) del(root->left);
+    if(root->right) del(root->right);
+    delete root;
+}
+
+void print(Node* root, int indent = 0) {
+    if(root != NULL){
+                std::cout << (root->type == Operator ? "OP: " : "INT: ") << root->val << "\n";
+
+        if(root->left) print(root->left, indent + 4);
+        if(root->right) print(root->right, indent + 4);
+        if(indent) {
+            std::cout << std::setw(indent) << " ";
+        }
+
+    }
+}
 
 
 int main(int argc, char *argv[]){
-    std::vector<char*> args;
-
-    if(argc >= 2){
-        for(int i = 1; i < argc; i++){
-            args.push_back(argv[i]);
-        }
+    if(argc <= 1) {
+        return 0;
     }
+    std::string a = argv[1];
+    
 
-    for(auto &c : args){
-        std::cout << c << "\n";
-    }
+    Node* root = buildTree(a.rbegin(),a.rend());
+    std::cout << a << "\n";
+
+    print(root);
+
+    del(root);
+
+
+    
+    
 
     return 0;
 }
